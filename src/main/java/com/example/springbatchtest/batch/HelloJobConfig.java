74a53c1a5b4b77bf9f7054fb665b23c608c780ac
seosang-won <zmdk1205@naver.com>
2024-10-11 15:42:16 +0900

    refactor : 빈 변수 이름 맞춰주기

--- src/main/java/com/example/springbatchtest/batch/HelloJobConfig.java
+++ src/main/java/com/example/springbatchtest/batch/HelloJobConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -17,19 +18,20 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class HelloJobConfig {
     @Bean
-    public Job helloJob(JobRepository jobRepository, Step simpleStep1) {
+    public Job helloJob(JobRepository jobRepository, Step helloStep1) {
         return new JobBuilder("helloJob", jobRepository)
-                .start(simpleStep1)
+                .start(helloStep1)
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
     @Bean
-    public Step helloStep1(JobRepository jobRepository, Tasklet helloStep1Tasklet1, PlatformTransactionManager platformTransactionManager) {
-        return new StepBuilder("helloStep1Tasklet1", jobRepository)
-                .tasklet(helloStep1Tasklet1, platformTransactionManager)
+    public Step helloStep1(JobRepository jobRepository, Tasklet helloStep1Tasklet, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("helloStep1Tasklet", jobRepository)
+                .tasklet(helloStep1Tasklet, platformTransactionManager)
                 .build();
     }
     @Bean
-    public Tasklet helloStep1Tasklet1() {
+    public Tasklet helloStep1Tasklet() {
         return ((contribution, chunkContext) -> {
             log.info("Hello World");
             System.out.println("Hello World");
